f5a300d0fd6f8301d4f2c0f00a5a3a0869e8eed7
Haeun <leehailey94@gmail.com>
2022-10-20 15:07:06 +0900

    modified moive_com table

--- Project_GTV/src/main/webapp/resources/SQL/reviews.sql
+++ Project_GTV/src/main/webapp/resources/SQL/reviews.sql
@@ -1,11 +1,19 @@
 create table movie_com(
-    user_no number(38)not null,
-    user_id varchar2(50) primary key,
-    mov_no number(38)not null,
-    mov_name varchar2(100)not null,
-    cont_com varchar2(500) not null,
+    mov_no number(38) primary key,
+    mov_name varchar2(100) not null,
+    user_id varchar2(50), --나중에 외래키로 가져와야함
+    cont_com varchar2(500),
     upload_com date,
     edit_com date
 );
 
 select * from movie_com order by user_id desc;
+drop table movie_com;
+
+--mov_no_seq 시퀀스 만들기
+create sequence mov_no_seq
+start with 1
+increment by 1
+nocache;
+
+select mov_no_seq.nextval from dual;
