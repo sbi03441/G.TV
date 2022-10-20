1316b045bfd1c40bbb200df9abc63557fa670983
JinHo <sbi03441@gmail.com>
2022-10-20 21:50:06 +0900

    last commit

--- Project_GTV/src/main/java/com/gtv/controller/ScreeningController.java
+++ Project_GTV/src/main/java/com/gtv/controller/ScreeningController.java
@@ -1,12 +1,16 @@
 package com.gtv.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.gtv.service.ScreeningService;
+import com.gtv.vo.ScreeningVO;
 
 @Controller
 public class ScreeningController {
@@ -19,8 +23,14 @@ public class ScreeningController {
 		ModelAndView m = new ModelAndView();
 		m.setViewName("reserve/screening");
 		return m;
+	};
+	
+	@PostMapping("/screening_ok")
+	public ModelAndView screening_ok(HttpServletRequest request, HttpServletResponse response, ScreeningVO sv) {
+		return null;
 	}
 	
+
 	
 	
 	
